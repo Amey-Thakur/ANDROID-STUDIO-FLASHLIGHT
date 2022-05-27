34180cfee7596f3381358f0cf922c39bcfb67e16
MegaSatish <mega.modha@gmail.com>
2022-05-27 15:08:32 +0530

    Flashlight

---
+++ Flashlight/app/src/main/java/android/nachiketa/flashlight/MainActivity.java
@@ -0,0 +1,63 @@
+package android.nachiketa.flashlight;
+
+import android.content.Context;
+import android.content.Intent;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    boolean isTorchOn = false;
+    ImageView imageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imageView = findViewById(R.id.img);
+
+    }
+
+    public void toggle(View view) {
+        Button button = (Button) view;
+        if (button.getText().equals("Switch On")) {
+            button.setText(R.string.switch_off_text);
+            imageView.setImageResource(R.drawable.flashoff);
+            torchToggle("on");
+        } else {
+            button.setText(R.string.switch_on_text);
+            imageView.setImageResource(R.drawable.flashon);
+            torchToggle("off");
+        }
+    }
+
+    private void torchToggle(String command) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            CameraManager camManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+            String cameraId = null; // Usually back camera is at 0 position.
+            try {
+                if (camManager != null) {
+                    cameraId = camManager.getCameraIdList()[0];
+                }
+                if (camManager != null) {
+                    if (command.equals("on")) {
+                        camManager.setTorchMode(cameraId, true);   // Turn ON
+                        isTorchOn = true;
+                    } else {
+                        camManager.setTorchMode(cameraId, false);  // Turn OFF
+                        isTorchOn = false;
+                    }
+                }
+            } catch (CameraAccessException e) {
+                e.getMessage();
+            }
+        }
+    }
+}
